3b45b0d8bebbe18474e778b2712c22014739c1e6
sweta <swetayadav2060@gmail.com>
2025-10-11 01:42:23 +0530

    updated project 2

--- PROJECT_02/script.js
+++ PROJECT_02/script.js
@@ -1,5 +1,25 @@
-const from=document.querySelector('form')
-from.addEventListener('submit',function(e){
-e.preventDefault()
-document.querySelector('#height').value
-})
+const form=document.querySelector('form');
+//this usecase will give you empty
+//const height=parseint(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const results=document.querySelector('#results');
+    if(height ==='' || height<0 || isNaN(height)){
+        results.innerHTML=`please enter a valid height ${height}`;
+        
+    }else if(weight===''|| weight<0 || isNaN(weight)){
+        results.innerHTML=`please enter a valid weight ${weight}`;
+        
+    }else {
+    
+        const bmi=(weight/(height*height/10000)).toFixed(2)
+        results.innerHTML=`<span>${bmi}</span>`
+        
+    
+    }
+});
