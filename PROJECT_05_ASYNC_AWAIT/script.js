1aba253a351006032a5823d0399ba4f509e7c76a
sweta <swetayadav2060@gmail.com>
2025-10-13 23:05:07 +0530

    added project5

---
+++ PROJECT_05_ASYNC_AWAIT/script.js
@@ -0,0 +1,30 @@
+//generate a random color
+const randomColor=function(){
+const hex='0123456789ABCDEF';
+let color='#';
+for(let i =0;i<6;i++){
+    color += hex[Math.floor(Math.random()*16)];
+}
+return color;
+};
+let intervalid;
+
+//console.log(randomColor());
+const startchangingColor=function(){
+    if(!intervalid){
+    intervalid=setInterval(changeBgColor,1000);
+    }
+    function changeBgColor(){
+        document.body.style.backgroundColor=randomColor();
+    }
+
+};
+const stopchangingColor=function(){
+    clearInterval(intervalid);
+    intervalid=null;
+}
+
+document.querySelector('#start').addEventListener('click',startchangingColor);
+
+document.querySelector('#stop').addEventListener('click',stopchangingColor);
+
